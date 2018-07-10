41fe328a7de49afe30f7d24e508ad2f18bc7301b
codingDiana <41088969+codingDiana@users.noreply.github.com>
2018-07-10 15:24:43 -0700

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,10 @@
+//Javascript for my Webpage
+
+// function myFunction(){
+//     alert("Page is loaded")
+// }
+
+
+function changeImage(){
+  document.getElementById("Zendaya").src = "beyonce.jpg"
+}
